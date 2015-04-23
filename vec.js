87ab93fbe92b5c529cdf04181cd9d2c43d8a7530
Nikita Prokopov <prokopov@gmail.com>
2015-04-23 14:42:42 +0300

    comments

--- vec.js
+++ vec.js
@@ -1,5 +1,8 @@
 // UTILS
 
+
+// Multimethods are flexible polymorphic construct, decoulped from class, open for extension
+
 function defmulti(name, dispatch_fn) {
   var dispatch_table_name = "_" + name + "_mm";
   window[dispatch_table_name] = {};
@@ -14,6 +17,8 @@ function defmethod(name, dispatch_value, fn) {
   window["_" + name + "_mm"][dispatch_value] = fn;
 }
 
+
+// Atom is a mutable reference with listeners (watchers)
 
 function atom(value) {
   return {value: value,
@@ -32,56 +37,89 @@ function reset(atom, value) {
   }
 }
 
+
+// Some shortcuts to ease immutable structs construction
+
 var list = function() { return Immutable.List.of.apply(Immutable.List, arguments); },
     map  = function() { return Immutable.Map.apply(Immutable, arguments); },
     set  = function() { return Immutable.Set.of.apply(Immutable.Set, arguments); },
     eq   = function(a, b) { return Immutable.is(a,b); };
 
-// MODEL
+
+// Tracking window size
+
+function current_viewport() {
+  return map({w: document.body.clientWidth, h: document.body.clientHeight});
+}
+
+window.addEventListener("resize", function(e) { 
+  reset(world_ref, world_ref.value.set("viewport", current_viewport()));
+} );
+
+
+// Compares set of immutable props via Immutable.is() — fast pointer comparison
+function should_update(name, from, to, props) {
+  for (var i = 0; i < props.length; ++i) {
+    var prop = props[i];
+    if (!eq(from[prop], to[prop])) {
+      // console.log(name + "." + prop + " changed");
+      return true;
+    }
+  };
+  // console.log(name + " // skip");
+  return false;
+}
+
+
+// HISTORY
+
+// Everything we render is kept in a single atom, including auxiliary stuff like click positions
+// and window size
+var world_ref = atom(new_world());
 
 function new_world() {
   var empty_model = map({ tool:      "select",
                           figures:   list(),
                           selection: set() });
-  return map({ history:   list(empty_model),
-               at:        0,
-               click_pos: undefined,
-               drag_pos:  undefined,
+  return map({ history:   list(empty_model), // collection of models
+               at:        0,                 // position in history
                viewport:  current_viewport() });
 }
 
-var world_ref = atom(new_world());
 
+// Model we’re looking at right now
 function current_model(w) {
   var world = w || world_ref.value;
   return world.get("history").get(world.get("at"));
 }
 
-function push_history(model) {
-  var history = world_ref.value.get("history"),
-      at      = world_ref.value.get("at"),
-      new_history = history.setSize(at+1).push(model),
-      new_at  = at+1;
-  
-  var skip = new_history.size - 50;
-  if (skip > 0) {
-    new_at -= skip;
-    new_history = new_history.skip(skip);
-  }
 
-  reset(world_ref, world_ref.value.set("history", new_history).set("at", new_at));
-}
-
-
+// Edit “mutates” current model, either pushing new model to the history or replacing it inplace
+// All changes to model go through this place only
 function edit_model(model) {
+  // create new history record only if figures have changed
   if (!eq(model.get("figures"), current_model().get("figures"))) {
-    push_history(model);
+    var history = world_ref.value.get("history"),
+        at      = world_ref.value.get("at"),
+        new_history = history.setSize(at+1).push(model),
+        new_at  = at+1;
+    
+    // limiting history size
+    var skip = new_history.size - 50;
+    if (skip > 0) {
+      new_at -= skip;
+      new_history = new_history.skip(skip);
+    }
+
+    reset(world_ref, world_ref.value.set("history", new_history).set("at", new_at));
   } else {
     var world = world_ref.value;
     reset(world_ref, world.setIn(["history", world.get("at")], model));
   }
 }
 
+
+// Note how plain dead trivial undo/redo are
 function undo() {
   var at = world_ref.value.get("at");
   if (at > 0)
@@ -95,44 +133,10 @@ function redo() {
     reset(world_ref, world_ref.value.set("at", at+1));
 }
 
-function persist(world) {
-  localStorage.setItem("vec/world", JSON.stringify(world_to_js(world)));
-}
 
-function world_to_js(world) {
-  var cache = list(),
-      obj_id = function (o) {
-                 var idx = cache.indexOf(o);
-                 if (idx === -1) {
-                   cache = cache.push(o);
-                   return cache.size - 1;
-                 } else
-                   return idx;
-               },
-      history = [];
-  world.get("history").forEach(function(model, i) {
-    history.push({ tool:      model.get("tool"),
-                   selection: model.get("selection").map(obj_id).toArray(),
-                   figures:   model.get("figures").map(obj_id).toArray() });
-  });
-  return { history: history,
-           at:      world.get("at"),
-           figures: cache.map(function(o) { return o.toJS(); }).toArray() };
-}
-
-function world_from_js(json) {
-  var figures = Immutable.List(json.figures).map(Immutable.Map),
-      get_fig = function(i) { return figures.get(i); },
-      history = Immutable.List(json.history).map(function(m) {
-        return map({
-          tool:      m.tool,
-          selection: Immutable.List(m.selection).map(get_fig),
-          figures:   Immutable.List(m.figures).map(get_fig)
-      })});
-  return new_world().set("history", history).set("at", json.at);
-}
 
 // FIGURES
+// mostly trivial code, some math and stuff
 
 defmulti("render_figure",  function(fig, selected, key) { return fig.get("type"); });
 defmulti("inside_figure",  function(fig, point)         { return fig.get("type"); });
@@ -307,18 +311,21 @@ defmethod("move_figure", "line", function(fig, delta) {
 });
 
 
-// TOOLBAR
+// TOOLS
 
-var tool_keys = list(
-  ["select", "V"],
-  ["rect",   "R"],
-  ["oval",   "O"],
-  ["line",   "L"]
-);
+// round mouse position for “snap to grid” behaviour
+function mouse_pos(e) {
+  return map({x: 10 * Math.round(e.clientX / 10),
+              y: 10 * Math.round(e.clientY / 10)});
+}
 
+// These multimethods take current model and return new model
+// They are pure and don’t mutate anything
+// This allows us to use them for speculative rendering
 defmulti("tool_on_click", function(tool, model, point, e)  { return tool; });
 defmulti("tool_on_drag",  function(tool, model, p1, p2, e) { return tool; });
 
+// Select changes “selection” set of a model
 defmethod("tool_on_click", "select",
   function(tool, model, point, e) {
     var fig       = find_selected(model.get("figures"), point),
@@ -334,6 +341,7 @@ defmethod("tool_on_click", "select",
       return model.set("selection", set());
   });
 
+// or moves figures
 defmethod("tool_on_drag", "select",
   function(tool, model, p1, p2, e) {
     var delta     = map({x: p2.get("x") - p1.get("x"), y: p2.get("y") - p1.get("y")}),
@@ -349,7 +357,6 @@ defmethod("tool_on_drag", "select",
     }
 
     if (find_selected(selection, p1) !== undefined) {
-      document.body.style.cursor = "move";
       return model
              .set("figures", scene.map(function(fig) {
                return selection.contains(fig) ? move_figure(fig, delta) : fig;
@@ -358,7 +365,7 @@ defmethod("tool_on_drag", "select",
     }
   });
 
-
+// figures just add new figure on drag
 function fig_drag_fn(tool, model, p1, p2, e) {
   if (!eq(p1, p2)) {
     var scene = model.get("figures");
@@ -372,7 +379,19 @@ defmethod("tool_on_drag", "rect", fig_drag_fn);
 defmethod("tool_on_drag", "oval", fig_drag_fn);
 defmethod("tool_on_drag", "line", fig_drag_fn);
 
+
+// REACT COMPONENTS
+
+var tool_keys = list(
+  ["select", "V"],
+  ["rect",   "R"],
+  ["oval",   "O"],
+  ["line",   "L"]
+);
+
+
 var Tool = React.createClass({
+  // Component will only be updated whenever props it depends on change
   shouldComponentUpdate: function(next_props) {
     return should_update("    Tool_" + this.props.code, this.props, next_props, ["code", "shortcut", "offset", "selected"]);
   },
@@ -393,6 +412,7 @@ var Tool = React.createClass({
   }
 });
 
+
 var Toolbar = React.createClass({
   shouldComponentUpdate: function(next_props) {
     return should_update("  Toolbar", this.props, next_props, ["tool"]);
@@ -408,66 +428,6 @@ var Toolbar = React.createClass({
 });
 
 
-// CANVAS
-
-function current_viewport() {
-  return map({w: document.body.clientWidth, h: document.body.clientHeight});
-}
-
-function mouse_pos(e) {
-  return map({x: 10 * Math.round(e.clientX / 10),
-              y: 10 * Math.round(e.clientY / 10)});
-}
-
-function canvas_mouse_down(e) {
-  if (world_ref.value.get("click_pos") === undefined)
-    reset(world_ref, world_ref.value.set("click_pos", mouse_pos(e)));
-}
-
-function canvas_mouse_move(e) {
-  var click_pos = world_ref.value.get("click_pos"),
-      drag_pos  = world_ref.value.get("drag_pos"),
-      pos       = mouse_pos(e);
-  if (click_pos !== undefined && 
-      (drag_pos !== undefined || !eq(click_pos, pos))) {
-    reset(world_ref, world_ref.value.set("drag_pos", pos).set("drag_event", e));
-  }
-}
-
-function canvas_mouse_up(e) {
-  var model = current_model(),
-      tool  = model.get("tool"),
-      click_pos = world_ref.value.get("click_pos"),
-      drag_pos  = world_ref.value.get("drag_pos"),
-      pos       = mouse_pos(e);
-
-  if (click_pos !== undefined) {
-    if (drag_pos !== undefined) {
-      var new_model = tool_on_drag(tool, model, click_pos, drag_pos, e);
-      if (new_model !== undefined)
-        edit_model(new_model);
-    } else {
-      var new_model = tool_on_click(tool, model, click_pos, e);
-      if (new_model !== undefined)
-        edit_model(new_model);
-    }
-  }
-  document.body.style.cursor = "auto";
-  reset(world_ref, world_ref.value.delete("click_pos").delete("drag_pos").delete("drag_event"));
-}
-
-function should_update(name, from, to, props) {
-  for (var i = 0; i < props.length; ++i) {
-    var prop = props[i];
-    if (!eq(from[prop], to[prop])) {
-      // console.log(name + "." + prop + " changed");
-      return true;
-    }
-  };
-  // console.log(name + " // skip");
-  return false;
-}
-
 var Scene = React.createClass({
   shouldComponentUpdate: function(next_props) {
     return should_update("  Scene", this.props, next_props, ["figures", "selection"]);
@@ -479,6 +439,7 @@ var Scene = React.createClass({
     return React.createElement("g", {}, figures.map(render));
   }
 });
+
 
 var History = React.createClass({
   shouldComponentUpdate: function(next_props) {
@@ -496,29 +457,27 @@ var History = React.createClass({
                        y:           viewport.get("h") - 20,
                        width:       12,
                        height:      12,
-                       onClick:     function(e) { at = i; render_canvas(); },
-                       onMouseOver: function(e) { render_canvas(history.get(i)); },
-                       onMouseOut:  function(e) { render_canvas(); },
+                       onClick:     function(e) { 
+                                      reset(world_ref, world_ref.value.set("at", i));
+                                    },
+                       onMouseOver: function(e) { render_ui(world_ref.value.set("at", i)); },
+                       onMouseOut:  function(e) { render_ui(world_ref.value); },
                      });
                    };
     return React.createElement("g", { id: "history" }, this.props.history.map(render));
   }
 });
 
+
+// Rendering is top-down. Components implement shouldComponentUpdate to decide whether 
+// they should be updated on this particular change or not
 var UI = React.createClass({
   shouldComponentUpdate: function(next_props) {
-    return should_update("Canvas", this.props, next_props, ["world"]);
+    return should_update("UI", this.props, next_props, ["world"]);
   },
   render: function() {
-    var world     = this.props.world,
-        model     = current_model(world),
-        click_pos = world.get("click_pos"),
-        drag_pos  = world.get("drag_pos");
-    if (drag_pos !== undefined) {
-      var new_model = tool_on_drag(model.get("tool"), model, click_pos, drag_pos, world.get("drag_event"));
-      if (new_model !== undefined)
-        model = new_model;
-    }
+    var world = this.props.world,
+        model = current_model(world);
     return React.createElement("svg",
              { id: "canvas" },
              React.createElement(Toolbar, { tool:     model.get("tool") }),
@@ -535,31 +494,69 @@ function render_ui(world) {
   React.render(React.createElement(UI, { world: world }), document.body);
 }
 
-add_watch(world_ref, function(world, old, _new) { render_ui(_new); });
-add_watch(world_ref, function(world, old, _new) { persist(_new); });
+// We do not call render_ui explicitly. Every change gets applied to world_ref
+// and re-render fires from its watcher
+add_watch(world_ref, function(world, old, _new) {
+  render_ui(_new);
+});
 
-var stored = localStorage.getItem("vec/world");
 
-if (stored !== null) {
-  reset(world_ref, world_from_js(JSON.parse(stored)));
-} else {
-  list(
-    figure_from_bb("oval", [110, 115, 120, 125]),
-    figure_from_bb("oval", [130, 115, 140, 125]),
-    figure_from_bb("oval", [150, 115, 160, 125]),
-    figure_from_bb("line", [100, 140, 170, 140]),
-    figure_from_bb("line", [170, 140, 180, 110]),
-    figure_from_bb("line", [180, 110, 280, 110]),
-    figure_from_bb("line", [280, 110, 290, 140]),
-    figure_from_bb("line", [290, 140, 600, 140]),
-    figure_from_bb("line", [100, 180, 600, 180]),
-    figure_from_bb("rect", [100, 100, 600, 400])
-  ).forEach(function(fig) {
-    edit_model(current_model().set("figures", current_model().get("figures").push(fig)));
-  });
+// MOUSE HANDLING
+
+function canvas_mouse_down(e) {
+  if (world_ref.value.get("click_pos") === undefined)
+    reset(world_ref, world_ref.value.set("click_pos", mouse_pos(e)));
 }
 
-// KEYBOARD
+function canvas_mouse_move(e) {
+  var click_pos = world_ref.value.get("click_pos"),
+      drag_pos  = world_ref.value.get("drag_pos"),
+      pos       = mouse_pos(e);
+  if (click_pos !== undefined && 
+      (drag_pos !== undefined || !eq(click_pos, pos))) {
+    reset(world_ref, world_ref.value.set("drag_pos", pos));
+
+    // if we’re in the process of the dragging, we render not a model stored in history,
+    // but the one tool_on_drag() call return. This model is auxiliary and ephemeral, it is
+    // not stored anywhere. It’s used to show intermediate states of figures move or creation. 
+    // No other code knows anything about that. Everybody expects a model, and they get a model
+
+    var model = current_model(),
+        new_model = tool_on_drag(model.get("tool"), model, click_pos, pos, e);
+    if (new_model !== undefined)
+        render_ui(world_ref.value.setIn(["history", world_ref.value.get("at")], new_model));
+  }
+}
+
+function canvas_mouse_up(e) {
+  var model = current_model(),
+      tool  = model.get("tool"),
+      click_pos = world_ref.value.get("click_pos"),
+      drag_pos  = world_ref.value.get("drag_pos"),
+      pos       = mouse_pos(e);
+
+  // On mouse up we’re reusing tool_on_drag() and tool_on_click()
+  // to commit new model to the history
+  if (click_pos !== undefined) {
+    if (drag_pos !== undefined) {
+      var new_model = tool_on_drag(tool, model, click_pos, drag_pos, e);
+      if (new_model !== undefined)
+        edit_model(new_model);
+    } else {
+      var new_model = tool_on_click(tool, model, click_pos, e);
+      if (new_model !== undefined)
+        edit_model(new_model);
+    }
+  }
+  reset(world_ref, world_ref.value.delete("click_pos").delete("drag_pos"));
+}
+
+document.addEventListener("mousedown", canvas_mouse_down);
+document.addEventListener("mousemove", canvas_mouse_move);
+document.addEventListener("mouseup", canvas_mouse_up);
+
+
+// KEYBOARD HANDLING
 
 document.addEventListener("keydown", function(e) {
   if (!e.ctrlKey && !e.shiftKey && !e.metaKey) {
@@ -587,11 +584,72 @@ document.addEventListener("keydown", function(e) {
       break;
   }
 });
-document.addEventListener("mousedown", canvas_mouse_down);
-document.addEventListener("mousemove", canvas_mouse_move);
-document.addEventListener("mouseup", canvas_mouse_up);
-window.addEventListener("resize", function(e) { 
-  reset(world_ref, world_ref.value.set("viewport", current_viewport()));
-} );
+
+
+
+// LocalStorage-powered persistence
+// We’re using “smart” serialization: each figure is stored only once (cache),
+// and models are referencing figures by ids instead of serializing their body
+function world_to_js(world) {
+  var cache = list(),
+      obj_id = function (o) {
+                 var idx = cache.indexOf(o);
+                 if (idx === -1) {
+                   cache = cache.push(o);
+                   return cache.size - 1;
+                 } else
+                   return idx;
+               },
+      history = [];
+  world.get("history").forEach(function(model, i) {
+    history.push({ tool:      model.get("tool"),
+                   selection: model.get("selection").map(obj_id).toArray(),
+                   figures:   model.get("figures").map(obj_id).toArray() });
+  });
+  return { history: history,
+           at:      world.get("at"),
+           figures: cache.map(function(o) { return o.toJS(); }).toArray() };
+}
+
+function world_from_js(json) {
+  var figures = Immutable.List(json.figures).map(Immutable.Map),
+      get_fig = function(i) { return figures.get(i); },
+      history = Immutable.List(json.history).map(function(m) {
+        return map({
+          tool:      m.tool,
+          selection: Immutable.List(m.selection).map(get_fig),
+          figures:   Immutable.List(m.figures).map(get_fig)
+      })});
+  return new_world().set("history", history).set("at", json.at);
+}
+
+// Whenever interesting parts of the world change, we store it
+add_watch(world_ref, function(world, old, _new) {
+  if (!eq(old.get("history"), _new.get("history")) || !eq(old.get("at"), _new.get("at")))
+    localStorage.setItem("vec/world", JSON.stringify(world_to_js(_new)));
+});
+
+// load stored world or build initial built-in scene
+if (localStorage.getItem("vec/world") !== null) {
+  reset(world_ref, world_from_js(JSON.parse(localStorage.getItem("vec/world"))));
+} else {
+  list(
+    figure_from_bb("oval", [110, 115, 120, 125]),
+    figure_from_bb("oval", [130, 115, 140, 125]),
+    figure_from_bb("oval", [150, 115, 160, 125]),
+    figure_from_bb("line", [100, 140, 170, 140]),
+    figure_from_bb("line", [170, 140, 180, 110]),
+    figure_from_bb("line", [180, 110, 280, 110]),
+    figure_from_bb("line", [280, 110, 290, 140]),
+    figure_from_bb("line", [290, 140, 600, 140]),
+    figure_from_bb("line", [100, 180, 600, 180]),
+    figure_from_bb("rect", [100, 100, 600, 400])
+  ).forEach(function(fig) {
+    edit_model(current_model().set("figures", current_model().get("figures").push(fig)));
+  });
+}
+
+
+
 
 
